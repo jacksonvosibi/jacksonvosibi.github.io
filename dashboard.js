d6aa8d5eb8bc738c75ad35c01340203abebd3226
GANGYI JACKSON V. <jvosibi01@gmail.com>
2026-01-25 01:46:40 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,12 @@
+const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
+if (!user) {
+  window.location.href = "login.html";
+} else {
+  document.getElementById("clientName").innerText = user.name;
+}
+
+function logout() {
+  localStorage.removeItem("loggedInUser");
+  window.location.href = "login.html";
+}
