5f4a106535037ef0758d3503565bbb4db2ca040f
GANGYI JACKSON V. <jvosibi01@gmail.com>
2026-01-25 01:44:59 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,29 @@
+function register() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  users.push({ name, email, password });
+  localStorage.setItem("users", JSON.stringify(users));
+
+  alert("Registration successful!");
+  window.location.href = "login.html";
+}
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const user = users.find(u => u.email === email && u.password === password);
+
+  if (user) {
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Wrong login details");
+  }
+}
