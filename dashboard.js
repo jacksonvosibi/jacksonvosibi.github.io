d90529c51d85c80f869f4119295ca4bc78eb3d15
GANGYI JACKSON V. <jvosibi01@gmail.com>
2026-01-25 01:55:19 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -4,6 +4,11 @@ if (!user) {
   window.location.href = "login.html";
 } else {
   document.getElementById("clientName").innerText = user.name;
+
+  // Payment info (default)
+  document.getElementById("payStatus").innerText = user.paymentStatus || "Pending";
+  document.getElementById("service").innerText = user.service || "Not Assigned";
+  document.getElementById("amount").innerText = user.amount || "₦0 / 0 FCFA";
 }
 
 function logout() {
